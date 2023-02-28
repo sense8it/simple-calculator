5662465ca1f6f897af0f49228502436596054232
sense8it <seoman2015y@gmail.com>
2023-02-28 17:06:26 +0200

    v 1.0 - two arguments - four arithmetic operations

--- script/main.js
+++ script/main.js
@@ -1,1 +1,23 @@
-console.log('test start')
+document.querySelector("#result").onclick = function () {
+  const inp1 = +document.querySelector("#inp1").value;
+  const sel = document.querySelector("#sel").value;
+  const inp2 = +document.querySelector("#inp2").value;
+  let result;
+
+  if (sel === "add") {
+    result = inp1 + inp2;
+  }
+  if (sel === "sub") {
+    result = inp1 - inp2;
+  }
+  if (sel === "mul") {
+    result = inp1 * inp2;
+  }
+  if (sel === "div") {
+    result = inp1 / inp2;
+  }
+  
+  document.querySelector('#resultOutput').innerHTML = result;
+};
+
+
